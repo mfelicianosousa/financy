345b6d90a1c3038bac9dd3094f35ec9ef4aeabd4
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-12-15 10:46:33 -0400

    intercepting http requests with angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { inMemoryDatabase } from './in-memory-database';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(inMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]
