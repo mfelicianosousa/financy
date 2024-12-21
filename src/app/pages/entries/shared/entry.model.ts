cd00ac299a7f69eaecb00e5a42aaa2ff9642d6e1
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-12-21 11:55:25 -0400

    adding BaseResourceModel

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,5 +1,6 @@
+import { BaseResourceModel } from "../../../shared/models/base-resource.models";
 import { Category } from "../../categories/shared/category.model";
-export class Entry {
+export class Entry extends BaseResourceModel{
   constructor(
     public id?: number,
     public name?: string,
@@ -11,7 +12,9 @@ export class Entry {
     public categoryId?: number,
     public category?: Category
 
-  ){}
+  ){
+    super()
+  }
 
   static types = {
     expense: 'Despesa',
