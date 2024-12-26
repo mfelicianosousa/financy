9f1933e069b21423a0eea358260b67418dc1b054
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-12-26 08:05:50 -0400

    extrating navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,24 +3,29 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
 import { inMemoryDatabase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(inMemoryDatabase),
-
+    RouterModule,
   ],
   exports: [
     // share modules
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
